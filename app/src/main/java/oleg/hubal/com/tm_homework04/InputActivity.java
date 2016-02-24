c5decbb032132229d8c3b6d3661b829d5b5863ad
OlegCoder <hubal.oleg@gmail.com>
2016-02-24 22:06:56 +0200

    initial commit, views added

---
+++ app/src/main/java/oleg/hubal/com/tm_homework04/InputActivity.java
@@ -0,0 +1,16 @@
+package oleg.hubal.com.tm_homework04;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by User on 24.02.2016.
+ */
+public class InputActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input);
+    }
+}
