b2dd5b6bd6c45d0e5b3111324c213f4ccf7bda43
OlegCoder <hubal.oleg@gmail.com>
2016-02-25 00:00:25 +0200

    styles and themes added

--- app/src/main/java/oleg/hubal/com/tm_homework04/MainActivity.java
+++ app/src/main/java/oleg/hubal/com/tm_homework04/MainActivity.java
@@ -1,13 +1,17 @@
 package oleg.hubal.com.tm_homework04;
 
+import android.app.Activity;
+import android.content.res.Resources;
+import android.support.annotation.StyleRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.WhiteTheme);
         setContentView(R.layout.activity_main);
     }
 }
