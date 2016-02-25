5779e3949d862afb146ff1022cca2f47b2a73140
OlegCoder <hubal.oleg@gmail.com>
2016-02-25 13:24:30 +0200

    check values and calculator added

--- app/src/main/java/oleg/hubal/com/tm_homework04/Checker.java
+++
@@ -1,7 +0,0 @@
-package oleg.hubal.com.tm_homework04;
-
-/**
- * Created by User on 25.02.2016.
- */
-public class Checker {
-}
--- app/src/main/java/oleg/hubal/com/tm_homework04/MainActivity.java
+++ app/src/main/java/oleg/hubal/com/tm_homework04/MainActivity.java
@@ -1,14 +1,17 @@
 package oleg.hubal.com.tm_homework04;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
@@ -19,7 +22,10 @@ public class MainActivity extends Activity implements View.OnClickListener {
     Button btnCalc;
     TextView tvA, tvB, tvResult;
     EditText edtOper;
-    Bundle bundle;
+    int a, b;
+    String oper;
+    Context context;
+    int result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +33,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
         setTheme(themeValue);
         setContentView(R.layout.activity_main);
 
+        context = this;
         initializeVarilables();
         setListener();
     }
@@ -43,8 +50,38 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 startActivityForResult(intent1, bRequestCode);
                 break;
             case R.id.btn_calc_AM:
+                oper = edtOper.getText().toString();
+                if (checkValues(oper, tvA.getText().toString(), tvB.getText().toString())) {
+                    result = calculateValues(oper);
+                    tvResult.setText(String.valueOf(result));
+                }
+        }
+    }
 
-                break;
+    private int calculateValues(String oper) {
+        switch (oper) {
+            case "-":
+                return (a - b);
+            case "+":
+                return (a + b);
+            case "*":
+                return (a * b);
+            case "/":
+                return (a / b);
+            default:
+                return 0;
+        }
+    }
+
+    private boolean checkValues(String oper, String a, String b) {
+        if(oper.isEmpty() || a.isEmpty() || b.isEmpty()) {
+            Toast.makeText(this, "Заполните все поля", Toast.LENGTH_LONG).show();
+            return false;
+        } else if (!(oper.equals("-") || oper.equals("+") || oper.equals("/") || oper.equals("*"))) {
+            Toast.makeText(this, "Выбирете одну из операций:(+,-,*,/)",Toast.LENGTH_LONG).show();
+            return false;
+        } else {
+            return true;
         }
     }
 
@@ -52,12 +89,15 @@ public class MainActivity extends Activity implements View.OnClickListener {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (data == null ) {return;}
         String value = data.getStringExtra("value");
+
         switch (requestCode) {
             case aRequestCode:
                 tvA.setText(value);
+                a = Integer.parseInt(value);
                 break;
             case bRequestCode:
                 tvB.setText(value);
+                b = Integer.parseInt(value);
                 break;
         }
     }
@@ -87,6 +127,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
             } else {
                 themeValue = R.style.WhiteTheme;
             }
+
         }
     }
 
